89ab1bc1d1f08f53250025de290f47b60e69df57
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2020-03-30 00:21:59 +0200

    Add openbis

---
+++ app/openbis.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+from pybis import Openbis
+
+
+def establish_db_connection():
+
+    # Should be done during server startup or when calling a function to set or get data
+    o = Openbis(os.environ['OPENBIS_SERVER'], verify_certificates=False,
+                allow_http_but_do_not_use_this_in_production_and_only_within_safe_networks=True)
+    o.login(os.environ['OPENBIS_USER'], os.environ['OPENBIS_PASSWORD'])
+    return o
+
+
+def register_new_case(o, data):
+
+    sample = o.new_sample(
+        type='CASE',
+        space='DEFAULT',
+        experiment='/DEFAULT/COVID19/E1',
+        props=data,
+        parents=[],
+        children=[])
+    sample.save()
+
+    return sample.permId
+
+
+def update_case(o, data):
+
+    parent = o.get_sample('20170518112808649-52')  # we need to get this number from the user, should be in data
+    sample = o.new_sample(
+        type='CASE',
+        space='DEFAULT',
+        experiment='/DEFAULT/COVID19/E1',
+        props=data,
+        parents=[parent],
+        children=[])
+    sample.save()
+
+    return sample.permId
+
+
+def get_all_cases(o):
+
+    samples = o.get_samples(space='DEFAULT/COVID19', experiment='/DEFAULT/COVID19/E1')
+    samples_as_json = pd.io.json.json_normalize(samples.df)
+
+    return samples_as_json, samples.df
