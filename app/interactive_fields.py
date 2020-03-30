c4d0303e43c74378975747b711580a282410557c
garullie <e.garulli@gmail.com>
2020-03-30 15:22:29 +0200

    final

--- app/interactive_fields.py
+++ app/interactive_fields.py
@@ -1,25 +1,28 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import (StringField,
-                     TextAreaField,
                      SubmitField,
-                     PasswordField,
-                     DateField,
                      SelectField,
-                     FormField)
+                     FormField,
+                     FieldList)
 from wtforms.validators import (DataRequired,
-                                Email, ValidationError)
+                                Email, ValidationError, Regexp)
 
 countries = ['Afghanistan', 'Åland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, the Democratic Republic of the', 'Cook Islands', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'Macedonia, the Former Yugoslav Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestine, State of', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe']
 
 class Status(FlaskForm):
-    infected = SubmitField('a')
+    infected = SubmitField('Infected')
     immune = SubmitField('Immune')
     non_infected = SubmitField(' Non infected')
 
+class Proof(FlaskForm):
+    proof = StringField('Serial Number: ')
+    #recaptcha = RecaptchaField()
+    proceed = SubmitField('Proceed')
+
 
 class ContributeMore(FlaskForm):
-    yes = SubmitField('yes')
-    no = SubmitField('no')
+    yes = SubmitField('Yes')
+    no = SubmitField('No')
 
 class MoreInfo(FlaskForm):
 
@@ -27,42 +30,46 @@ class MoreInfo(FlaskForm):
         if field.data not in countries:
             raise ValidationError('Not a valid country.')
 
-    age = SelectField('Age', [DataRequired()],
-                        choices=[('young', '0-50 years old'),
+    age = SelectField('Age',
+                        choices=[('-','-'),('young', '0-50 years old'),
                                  ('old', '51-100 years old')])
-
-    job = SelectField('Job', [DataRequired()],
-                      choices=[('Wizard', 'Wizard'),
+    gender = SelectField('Gender',
+                        choices=[('-','-'),
+                                ('F', 'Female'),
+                                 ('M', 'Male'),
+                                 ('X', 'Other')])
+    job = SelectField('Job',
+                      choices=[('-','-'),('Wizard', 'Wizard'),
                                ('Witch', 'Witch'),
                                ('Muggle', 'Muggle')])
-    country = StringField("Country: ", [DataRequired(), validate_country])
-
-
+    country = StringField("Country: ", validators=[validate_country])
+    postcode = StringField("Post Code: ", validators=[])
 
     submit = SubmitField('Submit')
 
 class KeepInTouch(FlaskForm):
     email = StringField('E-mail: ', [
-        Email(message='Not a valid email address.'),
-        DataRequired()])
+        Email(message='Not a valid email address.')])
     submit = SubmitField('Submit')
 
 class QuestioningEverything(FlaskForm):
+    proof = FormField(Proof)
     infection_status = FormField(Status)
     contribution = FormField(ContributeMore)
     additional_info = FormField(MoreInfo)
 
 
-class Country(object):
 
-    def __init__(self, message=None):
-        self.message = message
-
-    def __call__(self, form, field):
-        value = field.data
-        message = self.message
-        if message is None:
-            message = field.gettext('Invalid country name.')
-
-        if value not in countries:
-            raise ValidationError(message)
+# class Country(object):
+#
+#     def __init__(self, message=None):
+#         self.message = message
+#
+#     def __call__(self, form, field):
+#         value = field.data
+#         message = self.message
+#         if message is None:
+#             message = field.gettext('Invalid country name.')
+#
+#         if value not in countries:
+#             raise ValidationError(message)
--- app/routes.py
+++ app/routes.py
@@ -6,14 +6,16 @@ from .interactive_fields import Status, ContributeMore, MoreInfo, KeepInTouch, Q
 from datetime import datetime as dt
 from .utils import get_data_from_json
 from .visualization import create_plot
+from .openbis import (establish_db_connection, get_all_cases, register_new_case, update_case)
 
 # @app.route('/')
 # def home():
 #     return render_template('index.html',
 #                            template='home-template')
 
-ID = dt.now().strftime('%c')
+#ID = dt.now().strftime('%c')
 #ID = session['_id']
+ID = 0000
 
 
 
@@ -23,40 +25,56 @@ def success():
                            template='success-template')
 
 
+def create_infofile(ID):
+    f = open("app/static/{}.json".format(ID),"w+")
+    f.write('{}')
+
 
 def save_info(ID, key, value):
-    with open("app/static/comments.json", 'r') as file:
+    with open("app/static/{}.json".format(ID), 'r') as file:
         comments = json.load(file)
-    if ID not in comments.keys():
-        comments[ID] = {}
-    comments[ID][key] = value
-    with open("app/static/comments.json", 'w') as outfile:
+    comments[key] = value
+    with open("app/static/{}.json".format(ID), 'w+') as outfile:
         json.dump(comments, outfile)
+
 
 @app.route('/status', methods=('GET', 'POST'))
 def status():
+
     form = QuestioningEverything()
+    if form.proof.validate(form):
+        if 'proof-proceed' in request.form:
+            create_infofile(ID)
+            save_info(ID=ID, key="testid", value=form.proof.data['proof'])
+            return render_template('status.html',
+                                   form=form,
+                                   template='form-template',
+                                   p="proof_submitted")
+
     if form.infection_status.validate(form):
         if 'infection_status-infected' in request.form:
             status = "infected"
-            save_info(ID=ID, key="status", value=status)
+            save_info(ID=ID, key="diagnostic", value=status)
             return render_template('status.html',
                                    form=form,
                                    template='base',
+                                   p="proof_submitted",
                                    s="status_registered")
         elif 'infection_status-immune' in request.form:
             status = "immune"
-            save_info(ID=ID, key="status", value=status)
+            save_info(ID=ID, key="diagnostic", value=status)
             return render_template('status.html',
                                    form=form,
                                    template='base',
+                                   p="proof_submitted",
                                    s="status_registered")
         elif 'infection_status-non_infected' in request.form:
             status = "non_infected"
-            save_info(ID=ID, key="status", value=status)
+            save_info(ID=ID, key="diagnostic", value=status)
             return render_template('status.html',
                                    form=form,
                                    template='base',
+                                   p="proof_submitted",
                                    s="status_registered")
 
     if form.contribution.validate(form):
@@ -64,6 +82,7 @@ def status():
             return render_template('status.html',
                                    form=form,
                                    template='base',
+                                   p="proof_submitted",
                                    s="status_registered",
                                    c="contribution_y")
         if 'contribution-no' in request.form:
@@ -73,7 +92,8 @@ def status():
         save_info(ID=ID, key="job", value=form.additional_info.data['job'])
         save_info(ID=ID, key="age", value=form.additional_info.data['age'])
         save_info(ID=ID, key="country", value=form.additional_info.data['country'])
-        return redirect(url_for('success'))
+        save_info(ID=ID, key="zip", value=form.additional_info.data['postcode'])
+        return redirect(url_for('success'), perm_id=get_data(ID))
 
 
     return render_template('status.html',
@@ -105,3 +125,14 @@ def index():
 @app.route('/terms')
 def terms():
     return render_template('terms.html', template='base')
+
+
+def get_data(ID):
+    with open("app/static/{}.json".format(ID), 'r') as file:
+        comments = json.load(file)
+    o = establish_db_connection()
+    perm_id = register_new_case(o=o, data=comments)
+    return perm_id
+
+
+
