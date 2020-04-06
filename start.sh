bfd352b15f6a01d8e5a1cadbc97fbb9b485f8349
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2020-04-06 16:17:40 +0200

    Update start script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 # start.sh
 
-export FLASK_APP=main.py
-export FLASK_DEBUG=1
+# export FLASK_APP=main.py
+# export FLASK_DEBUG=1
 export APP_CONFIG_FILE=config.py
-python flask run
+# python flask run
+flask run --port=8080
